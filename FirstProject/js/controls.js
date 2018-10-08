82bc6c798ed83a6ef54f2d8e59997f98a2c28b64
joaopftorres <33656940+joaopftorres@users.noreply.github.com>
2018-10-08 09:05:31 +0100

    rotate eixoY (mal)

--- FirstProject/js/controls.js
+++ FirstProject/js/controls.js
@@ -16,6 +16,7 @@ function onKeyDown(e) {
     case 69:  //E
     case 101: //e 
         scene.traverse(function (node) {
+            chairTop.rotateY(Math.PI / 1000);
             if (node instanceof THREE.AxisHelper) {
                 node.visible = !node.visible;
             }
